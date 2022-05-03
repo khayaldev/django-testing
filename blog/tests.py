3bc909c07a5180887b1f84dbef669ad73b151a48
khayaltech <khayaltech@gmail.com>
2022-05-03 17:14:03 +0400

    I have followed PEP8 style guide  for code quality

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from .models import Post
 # Create your tests here.
+
+
+class Testing(TestCase):
+    def setUp(self):
+        self.blog = Post.objects.create(title='django', author='khayal', slug='django')
+        return super().setUp()
+
+    def test_check(self):
+        d = self.blog
+        self.assertTrue(isinstance(d, Post))
+        self.assertEqual(str(d), 'django')
+
+    def tearDown(self):
+        return super().tearDown()
